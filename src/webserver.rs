2fa38a699f24160d35d7d52628be46bc9d2c8558
blissful <blissful@sunsetglow.net>
2020-12-08 16:47:55 -0500

    rearrange webserver files; no custom responder atm

--- src/webserver.rs
+++ src/webserver.rs
@@ -3,14 +3,19 @@ use crate::routes;
 use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
 use env_logger::Env;
+use tera::Tera;
 
 pub async fn start(opts: Start) -> std::io::Result<()> {
     let bind_addr = format!("{}:{}", &opts.host, &opts.port);
+    let templates_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/views/templates/**/*.html");
 
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
+        let tera = Tera::new(&templates_dir).unwrap();
+
         App::new()
+            .data(tera)
             .wrap(Logger::default())
             .service(routes::index)
             .service(routes::login)
--- src/webserver/mod.rs
+++
@@ -1,6 +0,0 @@
-mod app;
-mod responders;
-mod routes;
-
-pub use app::*;
-pub use routes::*;
